9a65a95ed5015d7a32ce07580b8c267256069b3b
Davi e Tal <davi.souza.abrantes@gmail.com>
2024-01-06 18:11:14 -0300

    Idle Animations

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -9,6 +9,7 @@ public class PlayerMovement : MonoBehaviour
 
     private readonly int moveX = Animator.StringToHash("MoveX");
     private readonly int moveY = Animator.StringToHash("MoveY");
+    private readonly int moving = Animator.StringToHash("Moving");
 
     private PlayerActions actions;
     private Rigidbody2D rigidBody2D;
@@ -45,8 +46,13 @@ public class PlayerMovement : MonoBehaviour
     private void ReadMovement()
     {
         moveDirection = actions.Movement.Move.ReadValue<Vector2>().normalized;
-        if (moveDirection == Vector2.zero) return;
+        if (moveDirection == Vector2.zero)
+        {
+            animator.SetBool(moving, false);
+            return;
+        }
 
+        animator.SetBool(moving, true);
         animator.SetFloat(moveX, moveDirection.x);
         animator.SetFloat(moveY, moveDirection.y);
     }
