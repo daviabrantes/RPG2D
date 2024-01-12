596c23740ec49f0b1ccf63c4c1ae1477f65ab5a5
Davi e Tal <davi.souza.abrantes@gmail.com>
2024-01-12 19:11:46 -0300

    Added Player Exp

---
+++ Assets/Scripts/Player/PlayerExp.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.U2D;
+using UnityEngine;
+
+public class PlayerExp : MonoBehaviour
+{
+    [Header("Config")]
+    [SerializeField] private PlayerStats stats;
+
+    public void AddExp(float amount)
+    {
+        stats.CurrentExp += amount;
+        while (stats.CurrentExp > stats.NextLevelExp)
+        {
+            stats.CurrentExp -= stats.NextLevelExp;
+            NextLevel();
+        }
+    }
+
+    private void NextLevel()
+    {
+        stats.Level++;
+        float currentExpRequired = stats.NextLevelExp;
+        float newNextLevelExp = 
+            Mathf.Round(currentExpRequired + stats.NextLevelExp 
+                * (stats.ExpMultiplier / 100f));
+        stats.NextLevelExp = newNextLevelExp;
+    }
+}
