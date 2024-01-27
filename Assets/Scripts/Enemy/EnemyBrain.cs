52c394b84f24af68ff5cc14b55b76b53490f149d
Davi e Tal <davi.souza.abrantes@gmail.com>
2024-01-27 17:30:11 -0300

    Added Chase Behavior

--- Assets/Scripts/Enemy/EnemyBrain.cs
+++ Assets/Scripts/Enemy/EnemyBrain.cs
@@ -7,7 +7,8 @@ public class EnemyBrain : MonoBehaviour
     [SerializeField] private string initState;
     [SerializeField] private FSMState[] states;
 
-    public FSMState CurrentState {  get; set; }
+    public FSMState CurrentState { get; set; }
+    public Transform Player { get; set; }
 
     private void Start()
     {
