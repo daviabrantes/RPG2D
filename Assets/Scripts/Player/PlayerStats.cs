8a8beb273ad89ea6fb66abd559a12f6f66e10ec1
Davi e Tal <davi.souza.abrantes@gmail.com>
2024-04-14 14:18:48 -0300

    Added Attributes

--- Assets/Scripts/Player/PlayerStats.cs
+++ Assets/Scripts/Player/PlayerStats.cs
@@ -2,6 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum AttributeType
+{
+    Strength,
+    Dexterity,
+    Intelligence
+}
+
 [CreateAssetMenu(fileName = "PlayerStats", menuName ="Player Stats")]
 public class PlayerStats : ScriptableObject
 {
@@ -27,6 +34,12 @@ public class PlayerStats : ScriptableObject
     public float CriticalChance;
     public float CriticalDamage;
 
+    [Header("Attributes")]
+    public int Strength;
+    public int Dexterity;
+    public int Intelligence;
+    public int AttributePoints;
+
     [HideInInspector] public float TotalExp;
     [HideInInspector] public float TotalDamage;
 
@@ -38,5 +51,12 @@ public class PlayerStats : ScriptableObject
         CurrentExp = 0f;
         NextLevelExp = InitialNextLevelExp;
         TotalExp = 0f;
+        BaseDamage = 2;
+        CriticalChance = 10;
+        CriticalDamage = 50;
+        Strength = 0;
+        Dexterity = 0;
+        Intelligence = 0;
+        AttributePoints = 0;
     }
 }
